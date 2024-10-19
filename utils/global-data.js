b82dd893966f4bcbd881647e3442867773a4290a
Hien Nguyen <heyvin.nouhengy@gmail.com>
2024-10-20 01:08:33 +0700

    Initial edits to the blog

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,7 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+  const name = 'A thought repository about everything possible';
+  const blogTitle = 'Hien (Lucas) Nguyen';
+  const footerText = 'All rights reserved.';
 
   return {
     name,
